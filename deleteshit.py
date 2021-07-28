47e55a185087bfb0b053677054a33188708439e0
Harshit Khandelwal <84710222+Kareshi0@users.noreply.github.com>
2021-07-28 11:48:41 +0530

    Update deleteshit.py

--- deleteshit.py
+++ deleteshit.py
@@ -27,31 +27,31 @@ async def delete_animebot_shit(client, message):
         await message.delete()
         await guessmessage.delete()
         
-@app.on_message(filters.chat(chat_id) & filters.user(1382346231) & filters.regex(pattern='Thank you for using animebot.'))
+@app.on_message(filters.chat(chat_id) & filters.user(1382346231) & filters.regex(pattern='^Thank.*Thanks..$'))
 async def delete_animebot_startmessage(client, message):
     message.delete()
 
-@app.on_message(filters.chat(chat_id) & filters.user(1382346231) & filters.regex(pattern='Join our updates'))
+@app.on_message(filters.chat(chat_id) & filters.user(1382346231) & filters.regex(pattern='^Join.*sumit$'))
 async def delete_animebot2_startmessage(client, message):
     message.delete()
 
 
-@app.on_message(filters.chat(chat_id) & filters.user(778490365) & filters.regex(pattern='Hello I\'m'))
+@app.on_message(filters.chat(chat_id) & filters.user(778490365) & filters.regex(pattern='^Hello.*[\u263a-\U0001f645]$'))
 async def delete_nepgear_startmessage(client, message):
     message.delete()
 
 
-@app.on_message(filters.chat(chat_id) & filters.user(1416903424) & filters.regex(pattern='Ara Ara!'))
+@app.on_message(filters.chat(chat_id) & filters.user(1416903424) & filters.regex(pattern='^Ara.*s$'))
 async def delete_akeno_startmessage(client, message):
     if 'Ping' in message.reply_markup.InlineKeyboardButton.text:
         message.delete()
  
 
-@app.on_message(filters.chat(chat_id) & filters.user(1031952739) & filters.regex(pattern='Hello!'))
+@app.on_message(filters.chat(chat_id) & filters.user(1031952739) & filters.regex(pattern='^Hello.*me$'))
 async def delete_quotly_startmessage(client, message):
     message.delete()
 
-@app.on_message(filters.chat(chat_id) & filters.user(1885165230) & filters.regex(pattern='Im awake and runnin\' perfectly!!'))
+@app.on_message(filters.chat(chat_id) & filters.user(1885165230) & filters.regex(pattern='^Im.*s$'))
 async def delete_ohto_startmessage(client, message):
     message.delete()
     
