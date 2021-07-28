20df81031d469da37725ad3eca4fd476d1e43e44
Harshit Khandelwal <84710222+Kareshi0@users.noreply.github.com>
2021-07-28 11:13:41 +0530

    Update deleteshit.py

--- deleteshit.py
+++ deleteshit.py
@@ -3,9 +3,13 @@ from pyrogram import Client, filters
 
 app = Client("my_session", api_id=os.environ['api_id'], api_hash=os.environ['api_hash'])
 
+chat_id = 1425336751
+
+
+
 guessmessage = None
 
-@app.on_message(filters.chat('animeryu') & filters.user(1382346231))
+@app.on_message(filters.chat(chat_id) & filters.user(1382346231))
 async def delete_animebot_shit(client, message):
     if message.photo:
         global guessmessage
@@ -22,6 +26,34 @@ async def delete_animebot_shit(client, message):
         await message.reply_to_message.delete()
         await message.delete()
         await guessmessage.delete()
+        
+@app.on_message(filters.chat(chat_id) & filters.user(1382346231) & (filters.regex(pattern='Thank you for using animebot.') | filters.regex(pattern='Join our updates')))):
+async def delete_animebot_startmessage(client, message):
+    message.delete()
 
+@app.on_message(filters.chat(chat_id) & filters.user(778490365) & filters.regex(pattern='Hello I\'m')):
+async def delete_nepgear_startmessage(client, message):
+    message.delete()
+
+
+@app.on_message(filters.chat(chat_id) & filters.user(1416903424) & filters.regex(pattern='Ara Ara!')):
+async def delete_akeno_startmessage(client, message):
+    if 'Ping' in message.reply_markup.InlineKeyboardButton.text:
+        message.delete()
+ 
+
+@app.on_message(filters.chat(chat_id) & filters.user(1031952739) & filters.regex(pattern='Hello!')):
+async def delete_quotly_startmessage(client, message):
+    message.delete()
+
+@app.on_message(filters.chat(chat_id) & filters.user(1885165230) & filters.regex(pattern='Im awake and runnin\' perfectly!!')):
+async def delete_ohto_startmessage(client, message):
+    message.delete()
+    
+    
+
+
+    
+    
     
 app.run()
