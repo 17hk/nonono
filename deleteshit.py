aca5901b0e2b102fe012304cf2ad65660bc57c40
Harshit Khandelwal <84710222+Kareshi0@users.noreply.github.com>
2021-07-29 23:26:20 +0530

    Update deleteshit.py

--- deleteshit.py
+++ deleteshit.py
@@ -12,14 +12,12 @@ async def delete_animebot_shit(client, message):
         guessmessage = message
 
     elif message.text == 'That\'s not right!!!' or message.text == 'Eww small pp and small guesses are not allowed!!!' or message.text == 'Already Guessing!!!' or 'Oops you ran out of time...' in message.text:
-        await message.reply_to_message.delete()
         await message.delete()
     
     elif 'Not guessing anything right now' in message.text:
         await message.delete()
     
     elif 'UwU you got that right!!!' in message.text:
-        await message.reply_to_message.delete()
         await message.delete()
         await guessmessage.delete()
         
