07e7410e82bc06f9ea16d24def2155009ec8f2dd
Harshit Khandelwal <84710222+Kareshi0@users.noreply.github.com>
2021-07-29 22:45:27 +0530

    Update deleteshit.py

--- deleteshit.py
+++ deleteshit.py
@@ -27,6 +27,9 @@ async def delete_animebot_shit(client, message):
 async def delete_random_shit(client, message):
     if message.text.lower() == '/guess' or message.text.lower() == '/guess@any_animebot':
         await message.delete()
+    
+    elif message.text[:3].lower() == '/uwu':
+        await message.delete()
 
     
 app.run()
